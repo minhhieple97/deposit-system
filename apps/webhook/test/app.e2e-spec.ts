5bb9288f4170be2333dae2647b13cec7f920ef3b
hieple <hieplevuc@gmail.com>
2024-08-20 21:52:49 +0700

    Setup database schema

--- apps/webhook/test/app.e2e-spec.ts
+++ apps/webhook/test/app.e2e-spec.ts
@@ -1,14 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { WebhookModule } from './../src/webhook.module';
 
-describe('AppController (e2e)', () => {
+describe('WebhookController (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [WebhookModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
